d3c29982a139a937a5811af9b69623a37c96844d
Momin Khan <foolmoron@gmail.com>
2018-04-21 02:17:17 -0400

    10000 dotz

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -16,7 +16,7 @@ public static class Main {
     public static void BeforeSceneLoad() {
         var em = World.Active.GetOrCreateManager<EntityManager>();
 
-        DotArchetype = em.CreateArchetype(typeof(TransformMatrix));
+        DotArchetype = em.CreateArchetype(typeof(Translate2D), typeof(Rotate2D), typeof(Scale2D), typeof(TransformMatrix));
     }
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
@@ -25,10 +25,11 @@ public static class Main {
 
         DotRenderer = GameObject.Find("Prototypes/Dot").GetComponent<MeshInstanceRendererComponent>().Value;
 
-        for (int x = 0; x < 10; x++) {
-            for (int y = 0; y < 10; y++) {
+        for (int x = 0; x < 100; x++) {
+            for (int y = 0; y < 100; y++) {
                 var dot = em.CreateEntity(DotArchetype);
-                em.SetComponentData(dot, new TransformMatrix { Value = new float2(x * 1.1f + Mathf.Sin(y * 0.8f) * 0.5f - 5f, y * 1.1f + Mathf.Sin(x * 0.8f) * 0.5f - 5f).toMatrix() });
+                em.SetComponentData(dot, new Translate2D { Value = new float2(x * 0.1f + Mathf.Sin(y * 0.22f) * 0.1f - 5f, y * 0.1f + Mathf.Sin(x * 0.19f) * 0.1f - 5f) });
+                em.SetComponentData(dot, new Scale2D { Value = new float2(0.1f, 0.1f) });
                 em.AddSharedComponentData(dot, DotRenderer);
             }
         }
---
+++ Assets/Scripts/TRSSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using Unity.Transforms2D;
+using UnityEngine;
+
+public class TRSSystem : ComponentSystem {
+
+    struct Transforms {
+        public int Length;
+        [ReadOnly] public ComponentDataArray<Translate2D> Translate;
+        [ReadOnly] public ComponentDataArray<Rotate2D> Rotate;
+        [ReadOnly] public ComponentDataArray<Scale2D> Scale;
+        public ComponentDataArray<TransformMatrix> Matrix;
+    }
+
+    [Inject] Transforms transforms;
+
+    protected override void OnUpdate() {
+        for (int i = 0; i < transforms.Length; i++) {
+            transforms.Matrix[i] = new TransformMatrix { Value = 
+                new float4x4(
+                    transforms.Scale[i].Value.x, 0, 0, 0,
+                    0, transforms.Scale[i].Value.y, 0, 0,
+                    0, 0, 1, 0,
+                    transforms.Translate[i].Value.x, transforms.Translate[i].Value.y, 0, 1
+                )
+            };
+        }
+    }
+}
