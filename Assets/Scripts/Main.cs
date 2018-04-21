d267810044d6b505f416afbc20958c94ee0d6bb0
Momin Khan <foolmoron@gmail.com>
2018-04-21 01:57:31 -0400

    dot grid rendering

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using Unity.Transforms2D;
+using UnityEngine;
+
+public static class Main {
+
+    public static EntityArchetype DotArchetype;
+    public static MeshInstanceRenderer DotRenderer;
+    
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    public static void BeforeSceneLoad() {
+        var em = World.Active.GetOrCreateManager<EntityManager>();
+
+        DotArchetype = em.CreateArchetype(typeof(TransformMatrix));
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    public static void AfterSceneLoad() {
+        var em = World.Active.GetOrCreateManager<EntityManager>();
+
+        DotRenderer = GameObject.Find("Prototypes/Dot").GetComponent<MeshInstanceRendererComponent>().Value;
+
+        for (int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y++) {
+                var dot = em.CreateEntity(DotArchetype);
+                em.SetComponentData(dot, new TransformMatrix { Value = new float2(x * 1.1f + Mathf.Sin(y * 0.8f) * 0.5f - 5f, y * 1.1f + Mathf.Sin(x * 0.8f) * 0.5f - 5f).toMatrix() });
+                em.AddSharedComponentData(dot, DotRenderer);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Util/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public static class Extensions {
+
+    public static float4x4 toMatrix(this float2 pos) {
+        return new float4x4(
+            1, 0, 0, 0,
+            0, 1, 0, 0,
+            0, 0, 1, 0,
+            pos.x, pos.y, 0, 1
+        );
+    }
+}
